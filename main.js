b131d0d6b095c131b72a996ed2b89dc2f6d3f605
Hieu <dthsync@gmail.com>
2020-02-02 03:57:30 +0300

    fix

--- main.js
+++ main.js
@@ -4,25 +4,26 @@ document.querySelector("#departDate").value = today;
 document.querySelector("#returnDate").value = today;
 
 // Make link from data that enter in the form
-function MakeLink(){
+function MakeLink() {
   var dateFrom, dateTo, begin, language, origin, destination, date, origin1, destination1, date1, currency, url;
   dateFrom = document.getElementById("departDate").value;
   dateTo = document.getElementById("returnDate").value;
   begin = "https://fly.vietnamairlines.com/dx/VNDX/#/matrix?journeyType=round-trip"
-  language = "&locale="+document.getElementById("language").value;
-  origin = "&origin="+document.getElementById("flyFrom").value;
-  destination = "&destination="+document.getElementById("flyTo").value;
-  date = "&ADT=1&CHD=0&INF=0&date="+dateFrom.substr(5, 2) + "-" + dateFrom.substr(8, 2); +"-" + dateFrom.substr(0, 4);
-  origin1 = destination;
-  destination1 = origin;
-  date1 = "&date1="+dateTo.substr(5, 2) + "-" + dateTo.substr(8, 2) + "-" + dateTo.substr(0, 4);
-  currency = "&promoCode=&class=Economy&pointOfSale="+document.getElementById("currency").value;
-  url = begin + language + origin1 + destination1 + date1 + origin2 + destination2 + date2 + currency;
+  language = "&locale=" + document.getElementById("language").value;
+  origin = "&origin=" + document.getElementById("flyFrom").value;
+  destination = "&destination=" + document.getElementById("flyTo").value;
+  date = "&ADT=1&CHD=0&INF=0&date=" + dateFrom.substr(5, 2) + "-" + dateFrom.substr(8, 2) +"-" + dateFrom.substr(0, 4);
+  origin1 = "&origin1=" + document.getElementById("flyTo").value;
+  destination1 = "&destination1=" + document.getElementById("flyFrom").value;
+  date1 = "&date1=" + dateTo.substr(5, 2) + "-" + dateTo.substr(8, 2) + "-" + dateTo.substr(0, 4);
+  currency = "&promoCode=&class=Economy&pointOfSale=" + document.getElementById("currency").value;
+  url = begin + language + origin + destination + date + origin1 + destination1 + date1 + currency;
   return url;
 }
 
 // On click send data from form via link
 function Search() {
-  window.open(MakeLink(), '_blank');
+  var url = MakeLink();
+  window.open(url, '_blank');
   return false;
 }
